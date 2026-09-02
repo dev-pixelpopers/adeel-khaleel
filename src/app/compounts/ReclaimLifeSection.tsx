7783094e4cbb05fc7da0dce76d86915ab96caef0
Arham khalid <107454585+MArhamKhalid@users.noreply.github.com>
2026-09-03 03:38:49 +0500

    Section Reclaim height changes

--- src/app/compounts/ReclaimLifeSection.tsx
+++ src/app/compounts/ReclaimLifeSection.tsx
@@ -97,10 +97,10 @@ export default function ReclaimLifeSection() {
     <>
       <div ref={containerRef} className="relative w-full ">
         <section
-          className="relative w-full h-[50vh] md:h-dvh bg-cover bg-center bg-no-repeat flex items-center justify-center py-16 px-5 sm:py-20 sm:px-8 md:px-12 text-center select-none overflow-hidden md:min-h-dvh  mt-[-7%]"
+          className="relative w-full h-[50vh] lg:h-[120vh] xl:h-dvh bg-cover bg-center bg-no-repeat flex items-center justify-center py-16 px-5 sm:py-20 sm:px-8 md:px-12 text-center select-none overflow-hidden xl:min-h-dvh  mt-[-7%]"
           style={{ backgroundImage: "url('/reclaimlife-bg.png')" }}
         >
-          <div ref={sectionRef} className="relative top-0 mx-auto flex flex-col items-center justify-center z-10 pt-[20px] sm:pt-[100px] md:pt-[130px] lg:pt-[160px] xl:pt-[200px]">
+          <div ref={sectionRef} className="relative top-0 mx-auto flex flex-col items-center justify-center z-10 pt-[20px] sm:pt-[100px] md:pt-[130px] lg:pt-[130px] xl:pt-[200px]">
             <h2
               ref={headingRef}
               className="text-white font-Adorage uppercase text-[30px] sm:text-[46px] md:text-[56px] lg:text-[70px] xl:text-[110px] 2xl:text-[130px] max-w-[360px] sm:max-w-[600px] md:max-w-[800px] lg:max-w-[1100px] xl:max-w-[1400px] 2xl:max-w-[1600px] font-normal leading-[1.05] drop-shadow-[0_4px_0px_#00000066]"
@@ -110,7 +110,7 @@ export default function ReclaimLifeSection() {
 
             <p
               ref={textRef}
-              className=" mt-5 sm:mt-6 md:mt-8 text-white/95 font-Matangi-Medium font-normal leading-relaxed text-base sm:text-lg md:text-2xl lg:text-[30px] xl:text-[40px] max-w-[350px] sm:max-w-[600px] md:max-w-[850px] lg:max-w-[1100px] xl:max-w-[1300px]"   
+              className=" mt-5 sm:mt-6 md:mt-8 text-white/95 font-Matangi-Medium font-normal leading-relaxed text-base sm:text-lg md:text-2xl lg:text-2xl xl:text-[40px] max-w-[350px] sm:max-w-[600px] md:max-w-[850px] lg:max-w-[1100px] xl:max-w-[1300px]"   
               >
               Providing Patients With Compassionate, Specialized <br className="hidden md:block" />
               Treatments for Optimal Recovery & Long-term Health
